e964b19e2f064f2009336f82e1c61b9c5bed4dec
renandeandradevaz <renanandrade_rj@hotmail.com>
2014-05-31 16:06:26 -0300

    afdafasf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   protect_from_forgery with: :exception
+  helper_method :formatar_data
 
   def set_locale
     I18n.locale = 'pt'
   end
+
+  def formatar_data(data)
+    data.strftime("%d/%m/%Y %H:%M")
+  end
 end
+
+
+
+

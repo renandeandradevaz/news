6a9e3d11f4648ab5f221026558aa036db13832fd
Renan Vaz <renanandrade_rj@hotmail.com>
2014-06-04 23:14:14 -0300

    Update teste-nightwatch.js

--- nightwatch_tests/teste-nightwatch.js
+++ nightwatch_tests/teste-nightwatch.js
@@ -1,7 +1,7 @@
 module.exports = {
   "Teste de pesquisa" : function (client) {
     client
-      .url("http://localhost:3000")
+      .url("http://news.renanvaz.info:3000")
       .setValue("#query","Recife 21h38 central da capital")
       .click(".botao-pesquisar")
       .pause(1000)

a106641ed8ed77eb655a801422cf325df0c1be3a
Renan Vaz <renanandrade_rj@hotmail.com>
2014-06-05 15:19:30 -0300

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
     $('#menu').on('click', function(e){
         
-        $('#menu').text('');
+        $('#menu').text('&nbsp;');
 
         if($('#selecione-categoria').find('option').length <= 1){
 
